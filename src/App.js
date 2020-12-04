17df64dcd0029a5871adf9d064b2cc53722e6b90
sfisher <s.fisher@alumni.ubc.ca>
2020-12-04 11:32:48 -0600

    ci with push

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello there! The CI REALLY Works, yay!</p>
+        <p>Hello there! The CI REALLY REALLY Works, yay!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
