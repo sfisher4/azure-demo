296ffc8cd5058f02ee568a6a049994f2c3c5ad95
sfisher <s.fisher@alumni.ubc.ca>
2020-12-04 10:58:45 -0600

    test CI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello there! This is a new build and deploy!</p>
+        <p>Hello there! The CI Works, yay!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
